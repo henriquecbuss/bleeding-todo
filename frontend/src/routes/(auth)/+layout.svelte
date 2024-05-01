519e1b4c3a157415e5fb86f2a155179704d99082
Henrique da Cunha Buss <henriquebusss@gmail.com>
2024-05-01 19:29:58 -0300

    Adjust for when there is no workspace

--- frontend/src/routes/(auth)/+layout.svelte
+++ frontend/src/routes/(auth)/+layout.svelte
@@ -4,7 +4,12 @@
 	import { authStore } from '$lib/stores/auth.store';
 </script>
 
-<RedirectGate condition={!$authStore} redirect={`/${$authStore?.workspaces[0].id}`}>
+<RedirectGate
+	condition={!$authStore}
+	redirect={$authStore && $authStore.workspaces.length > 0
+		? `/${$authStore.workspaces[0].id}`
+		: '/'}
+>
 	<div class="h-full flex flex-col bg-gray-50">
 		<LoggedOutNavbar />
 
